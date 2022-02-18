036a7758860d2cd5e6d58014bd2a0f0387655178
sg994 <simonegentile1994@yahoo.it>
2022-02-18 13:18:04 +0100

    progetto s4

---
+++ s4/g4/js/myscript.js
@@ -0,0 +1,9 @@
+let presentazione = 'Ciao a tutti mi chiamo Simone e sto studiando JS';
+var grande = presentazione.toUpperCase();
+var piccolo = presentazione.toLowerCase();
+console.log(grande);
+console.log(piccolo);
+var caratteri = presentazione.split('  ');
+console.log(caratteri);
+caratteri.log()
+
