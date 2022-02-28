e069ae641df535a8df225384be9f4a0d60f7c501
sg994 <simonegentile1994@yahoo.it>
2022-02-28 18:18:06 +0100

    correzione esercizio s6g1

--- s6/g1/js/script.js
+++ s6/g1/js/script.js
@@ -1,66 +1,46 @@
-// Quando il documento è pronto...   
 $(document).ready(function(){
- //gestisco il click sul pulsante Iscriviti
- $("#iscr-news").click(function(){
 
-    var nome = $("#nome").val()
-    if(nome == ""){
-        $("#nome").next('*Richiesto')
-    }else{
-        $("#nome").next().text('')
-    }
-    var email = $("#email-1").val()
-    if(nome == ""){
-        $("#email-1").next('*Richiesto')
-    }else{
-        $("#email-1").next().text('')
-    }   
-    var email2 = $("#email-2").val()
-    if(nome == ""){
-        $("#email2").next('*Richiesto')
-    }else{
-        $("#email2").next().text('')
-    } 
-    if(email !== email2){
-        $("email-2").next().text('*Le email devono essere uguali')
-    }else {
-        $("#email-2").next().text('')
-    }
-    if (nome == true && email == true && email2 == true){
-        $("#form-email").subit()
-    }
-    else{
-        alert('ricntrolla i dati')
-    }
+    $("#iscr-news").click(function () {
 
+     let controllo = true;
+     let email1 = $("#email-1").val();
+     let email2 = $("#email-2").val();
+
+     function riconosce (campo){
+         if($(campo).val() == ""){
+             $(campo).next().text("Campo richiesto");
+             controllo = false;
+         } else {
+             $(campo).next().text("");
+         }
+     }
+
+     let arrayy = $("#nome, #email-1, #email-2");
+
+     arrayy.each((indice,element) => {
+         riconosce(element);
+
+     });
+
+
+
+     if (email1 !== email2){
+         $("#email-2").next().text("Le email non corrispondono!");
+         controllo = false;
+     }
+
+     if(controllo == true){
+         alert ("form inviato");
+     } else {
+         alert ("controlla che tutti i campi siano corretti!!");
+     }
+
+
+    });
+
+    $("#pulisci-form").click(function () {
+     $("#nome, #email-1, email-2").val("");
+
+    });
 
  })
- $("#pulisci-form").click(function(){
-     $("#email-1").val('')
-     $("#nome").val('')
-     $("#email-2").val('')
-
- })
-
-
-    $()
-    
-        // inizializzo una variabile di controllo a true
-        
-        // utilizzo una dichiarazione if e se il valore di nome è presente altrimenti devo scrivere *Richiesto
-        
-
-        
-        // nell'else uso next e text per impostare l'elemento span adiacente
-        
-        // assegno a due variabili il valore dei due campi email
-        
-        // ripeto la condizione nel nome anche per l'email
-        
-        // nella verifica devo mettere una ulteriore condizione se le email inserite sono diverse
-
-        //invio del form se tutti i campi sono validi
-
-    //gestisco il click sul pulsante Pulisci il form, i campi devono essere vuoti e 
-    //posizionare un asterisco per identificare il campo obbligatorio. 
-})
